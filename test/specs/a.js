791fac193bd1c91ef658571c5774e21179b4fb21
Gareth Robinson <gareth.robinson@ratecity.com.au>
2016-09-15 12:39:35 +1000

    Add basic tests for home loans search page

--- test/specs/a.js
+++ test/specs/a.js
@@ -6,4 +6,13 @@ describe('RateCity website', function() {
         var title = browser.getTitle();
         assert.equal(title, 'Interest Rates Comparison, Financial News & Resources | Compare & Save @ RateCity');
     });
+
+    it('should have the right title - the fancy generator way', function () {
+        browser.url('http://www.ratecity.com.au/home-loans/mortgage-rates');
+        var title = browser.getTitle();
+        // assert.equal(title, 'Interest Rates Comparison, Financial News & Resources | Compare & Save @ RateCity');
+        browser.getText('td.productName', function(text) {
+          console.log(text);
+        })
+    });
 });
