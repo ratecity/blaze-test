39afb8dddcdd1f96ef38827a5d1a11bd50efc64f
Gareth Robinson <gareth.robinson@ratecity.com.au>
2016-09-16 21:12:12 +1000

    Endpoint to view previous hours

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+function getFilesFromHour(path) {
+  var files = fs.readdirSync(path);
+  files.sort(function(a, b) {
+      return fs.statSync(path + b).mtime.getTime() - fs.statSync(path + a).mtime.getTime();
+   });
+  return files.map(function(file) { return path + file }).map(function(file) {
+    var contents = fs.readFileSync(file, 'utf8')
+    if (contents.match(/9 passing/g)) {
+      return {result: 'passed', name: file}
+    } else {
+      return {result: 'failed', name: file}
+    }
+  })
+}
 
 function getFilesFromLastHour () {
   var paths = ['./runs'];
@@ -14,23 +28,26 @@ function getFilesFromLastHour () {
     paths.push(files[0])
   }
   var path = paths.join('/') + '/'
-  var files = fs.readdirSync(path);
-  files.sort(function(a, b) {
-      return fs.statSync(path + b).mtime.getTime() - fs.statSync(path + a).mtime.getTime();
-   });
-  return files.map(function(file) { return path + file }).map(function(file) {
-    var contents = fs.readFileSync(file, 'utf8')
-    if (contents.match(/11 passing/g)) {
-      return {result: 'passed', name: file}
-    } else {
-      return {result: 'failed', name: file}
-    }
-  })
+  return getFilesFromHour(path)
 }
 
 app.get('/runs/*', function (req, res) {
   var contents = fs.readFileSync('.' + req.url, 'ascii')
-  res.send(contents.replace(/\n/g, "<br/>").replace(/\[[0-9];*[0-9]*m/g, ''))
+  res.send(contents.replace(/\n/g, "<br/>").replace(/\[[0-9];*[0-9]*m/g, '').replace(/b\$/g, ''))
+})
+
+app.get('/previous/:month/:day/:hour', function (req, res) {
+  var path = './runs/' + req.params.month + '/' + req.params.day + '/' + req.params.hour + '/';
+  console.log(path)
+  str = '<html>';
+  getFilesFromHour(path).forEach(function(file) {
+    if (file.result === 'passed') {
+      str += '<a style="color: green" href="' + file.name + '">' + file.name + '</a><br/>'
+    } else {
+      str += '<a style="color: red" href="' + file.name + '">' + file.name + '</a><br/>'
+    }
+  })
+  res.send(str)
 })
 
 app.get('/', function (req, res) {
