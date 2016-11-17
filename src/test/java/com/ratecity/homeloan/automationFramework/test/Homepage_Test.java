434aea9963a5916a97a2126a6727316548685a5a
Deepak Gupta <deepak.gupta@tothenew.com>
2016-11-17 13:33:51 +0530

    added test cases for compoarison page

--- src/test/java/com/ratecity/homeloan/automationFramework/test/Homepage_Test.java
+++ src/test/java/com/ratecity/homeloan/automationFramework/test/Homepage_Test.java
@@ -3,7 +3,9 @@ package com.ratecity.homeloan.automationFramework.test;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.ratecity.homeloan.automationFramework.pages.HomeLoanComparisonpage;
 import com.ratecity.homeloan.automationFramework.pages.HomeLoanLandingpage;
+import com.ratecity.homeloan.automationFramework.pages.HomeLoanMortgageRates;
 import com.ratecity.homeloan.automationFramework.utilities.BaseClass;
 import com.ratecity.homeloan.automationFramework.utilities.Utility;
 import com.relevantcodes.extentreports.LogStatus;
@@ -38,139 +40,24 @@ public class Homepage_Test extends BaseClass {
 		  
 	  }	
 	
-/*	
-	@Test(priority=1)
-	  public void fn_HomeLoan_DisplayAtLeat_15Contents_TestOR(){
-		 // logger = reports.startTest("abcd");
-		  List<WebElement> list  = driver.findElements(parser.getobjectLocator("HomeLoan.ProductList"));
-		  if(list.size()==15){
-			  Assert.assertTrue(true);
-		  }
-		  else{
-		  Assert.assertTrue(false,list.size() + ": is getting displayed ");
-		  }
-	  }
-
- 
-  //@Test(priority=1)
-  public void fn_HomeLoan_DisplayAtLeat_15Contents(){
-	 // logger = reports.startTest("abcd");
-	  List<WebElement> list  = driver.findElements(By.cssSelector("tr.product"));
-	  if(list.size()==15){
-		  Assert.assertTrue(true);
-	  }
-	  else{
-	  Assert.assertTrue(false,list.size() + ": is getting displayed ");
-	  }
-  }
-  
- // @Test(priority=2)
-  public void fn_HomeLoan_DisplayAtLeat_50Contents() throws InterruptedException{
-	  Thread.sleep(1000);
-	  driver.findElement(By.cssSelector("div.rate-table-control span a")).click(); 
-	  Thread.sleep(5000);
-	  List<WebElement> list  = driver.findElements(By.cssSelector("tr.product"));
-	  if(list.size()==50){
-		  Assert.assertTrue(true);
-	  }
-	  else{
-	  Assert.assertTrue(false,list.size() + ": is getting displayed ");
-	  }
-  }
-  
-   //@Test
-  public void fn_HomeLoan_DisplayAtLeat_100Contents() throws InterruptedException{
-	  Thread.sleep(1000);
-	 
-	  WebElement show  = driver.findElement(By.cssSelector("div.rate-table-control span a"));
-	  System.out.println(show.getText());
-	  show.click();
-	  
-	  Thread.sleep(5000);
-	  List<WebElement> list  = driver.findElements(By.cssSelector("tr.product"));
-	  System.out.println("******************List size : "+ list.size());
-	  if(list.size()==100){
-		  Assert.assertTrue(true);
-	  }
-	  else{
-	  Assert.assertTrue(false,list.size() + ": is getting displayed ");
-	  }
-  }
- 
- //@Test(priority=3)
-  public void fn_toValidateBankPage() throws InterruptedException{
-	  
-	  WebElement firstbank =  driver.findElement(By.cssSelector("tr.product div a"));
-	  String bankname = firstbank.getText();
-	  firstbank.click();
-	  Thread.sleep(5000);
-	  if(RateBusterStandardVariable.fn_verifytitle()){
-		  WebElement bankheader =  driver.findElement(By.cssSelector("ol.breadcrumb li.active span[property='name']"));
-		  if(bankname.equalsIgnoreCase(bankheader.getText())){
-			 Assert.assertTrue(true);
-		  }
-		  else
-			  Assert.assertTrue(false, "Expected page is"+firstbank.getText()+" :- is  not getting displayed");  
-	  }
-	  else{
-		  
-		  Assert.assertTrue(false, "Wrong page: "+driver.getTitle()+ " is getting displayed"); 
-	  }
-	  
-	  
-  }
-  
- @Test(priority=2)
-  public void fn_toVerifyComparefunctionalityWith2Banks() throws Exception{
-	
-	 logger = report.startTest("CompareButtonFunctionality");
-	 HomeLoanMortgageRates.fn_ClickOnCompareCheckbox(2);
-	 HomeLoanMortgageRates.fn_ClickOnCompareButton();
-	 if(HomeLoanComparisonpage.fn_VerifyHomeLoanText()){
-		if(HomeLoanComparisonpage.fn_VerifyComparisontable()==3){
-			logger.log(LogStatus.PASS, "INTO METHOD==> CompareFunctionality :  Working Successfully!! :)");
-			 Assert.assertTrue(true); 
-		}
-		else{
-			 logger.log(LogStatus.FAIL, "INTO METHOD==> CompareFunctionality :  Not working Successfully!! (: ");
-			 Assert.assertTrue(false);
-		 }
+	@Test(priority=2)
+	  public void fn_toVerifyComparefunctionalityWith2Banks() throws Exception{
 		
-	 }
-	}
- 
- 
- @Test(priority=3)
- public void fn_toVerifyCompareWithBig4Button() throws Exception{
-	
-	 logger = report.startTest("CompareWithBig4ButtonFunctionality");
-	 HomeLoanMortgageRates.fn_ClickOnCompareCheckbox(1);
-	 HomeLoanMortgageRates.fn_ClickOnComparewithBig4Button();
-	 if(HomeLoanComparisonpage.fn_VerifyHomeLoanText()){
-		if(HomeLoanComparisonpage.fn_VerifyComparisontable()==6){
-			logger.log(LogStatus.PASS, "INTO METHOD==> CompareWithBig4Button Functionality :  Working Successfully!! :)");
-			 Assert.assertTrue(true); 
-		}
-		else{
-			 logger.log(LogStatus.FAIL, "INTO METHOD==> CompareWithBig4 Functionality :  Not working Successfully!! (: ");
-			 Assert.assertTrue(false);
+		 logger = report.startTest("CompareButtonFunctionality");
+		 BaseClass.getDriver().navigate().to("https://staging.ratecity.com.au/home-loans/mortgage-rates");
+		 Utility.GoToSleep(2000);
+		 HomeLoanMortgageRates.fn_ClickOnCompareCheckbox(2);
+		 HomeLoanMortgageRates.fn_ClickOnCompareButton();
+		 if(HomeLoanComparisonpage.fn_VerifyHomeLoanText()){
+			if(HomeLoanComparisonpage.fn_VerifyComparisontable()==3){
+				logger.log(LogStatus.PASS, "INTO METHOD==> CompareFunctionality :  Working Successfully!! :)");
+				 Assert.assertTrue(true); 
+			}
+			else{
+				 logger.log(LogStatus.FAIL, "INTO METHOD==> CompareFunctionality :  Not working Successfully!! (: ");
+				 Assert.assertTrue(false);
+			 }
+			
 		 }
-		
-	 }
-	}
- 
-  
-  
- // @Test(priority=5)
-  public void fn_toVerify_RateBusteredElements() throws Exception{
-	  WebElement firstbank =  driver.findElement(By.cssSelector("tr.product div a"));
-	  RateBusterStandardVariable.clickAndWait(firstbank, 2000);
-	 // Thread.sleep(5000);
-	  RateBusterStandardvariable_Test.fn_VerifyLoanReviews();
-	  Thread.sleep(30000);
-	  
-	  
-  }
-  
-*/  
+		}  
  }
