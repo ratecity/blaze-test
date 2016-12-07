280d4902961c05b6eef4efd32898fd0149272885
Deepak Gupta <deepak.gupta@tothenew.com>
2016-12-07 11:51:15 +0530

    ChangesDone to BaseClass to run CH

--- src/test/java/com/ratecity/homeloan/automationFramework/utilities/BaseClass.java
+++ src/test/java/com/ratecity/homeloan/automationFramework/utilities/BaseClass.java
@@ -87,40 +87,12 @@ public class BaseClass {
 	   // getDriver().get(System.getProperty("user.dir")+File.separator+"Reports"+File.separator+"AutomationReport : "+timeStamp+".html");
 		
 	}
-	 @BeforeClass
-	    public static void setUp() throws IOException {
-	        String travisCiFlag = System.getenv().get("TRAVIS");
-	        FirefoxBinary firefoxBinary = "true".equals(travisCiFlag)
-	                ? getFirefoxBinaryForTravisCi()
-	                : new FirefoxBinary();
-	 
-	        driver = new FirefoxDriver(firefoxBinary, new FirefoxProfile());
-	    }
-	 
-	    private static FirefoxBinary getFirefoxBinaryForTravisCi() throws IOException {
-	        String firefoxPath = getFirefoxPath();
-	       Logger staticLog = LoggerFactory.getLogger(BaseClass.class);
-	       staticLog.info("Firefox path: " + firefoxPath);
-	       System.out.println("***********************************"+firefoxPath);
-	 
-	        return new FirefoxBinary(new File(firefoxPath));
-	    }
-	 
-	    private static String getFirefoxPath() throws IOException {
-	        ProcessBuilder pb = new ProcessBuilder("which", "firefox");
-	        pb.redirectErrorStream(true);
-	        Process process = pb.start();
-	        System.out.println("*****************"+process.toString());
-	        try (InputStreamReader isr = new InputStreamReader(process.getInputStream(), "UTF-8");
-	             BufferedReader br = new BufferedReader(isr)) {
-	            return br.readLine();
-	        }
-	    }
+	
 
 	@BeforeMethod
 	  public void fn_Launchbrowser() throws IOException{
 		//  System.setProperty(Utility.fn_ReaddataFronPropFile("chromeDriver"), Utility.fn_ReaddataFronPropFile("chromeDriverpath"));
-		  driver  = new FirefoxDriver();
+		  driver  = new ChromeDriver();
 		  driver.manage().timeouts().implicitlyWait(Integer.parseInt(Utility.fn_ReaddataFronPropFile("implicit_wait")), TimeUnit.SECONDS);
 		  driver.manage().window().maximize();
 		  driver.manage().deleteAllCookies();
