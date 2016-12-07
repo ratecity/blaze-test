18e2d13a733fa6a01e955420a23283db44798bf5
Deepak Gupta <deepak.gupta@tothenew.com>
2016-12-07 16:59:33 +0530

    hvuvuvuujvjvjh

--- src/test/java/com/ratecity/homeloan/automationFramework/pages/HomeLoanMortgageRates.java
+++ src/test/java/com/ratecity/homeloan/automationFramework/pages/HomeLoanMortgageRates.java
@@ -33,6 +33,7 @@ public class HomeLoanMortgageRates {
 	
 		List<WebElement> we =  BaseClass.getDriver().findElements(new RespositoryParser().
 				getobjectLocator("HomeLoan.MortgageRate"));
+		System.out.println("**************From Compare CheckBox******");
 		for (int i=0;i<value;i++) {
 			Utility.scrollToElement(we.get(i));
 			Utility.selectcheckbox(we.get(i));
